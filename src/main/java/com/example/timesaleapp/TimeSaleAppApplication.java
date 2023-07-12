6e0c6a8ca490b4ae8a27ff022fca8e4b721dd1d8
HandmadeCloud <joje1003@gmail.com>
2023-07-12 21:50:34 +0900

    project init

---
+++ src/main/java/com/example/timesaleapp/TimeSaleAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.timesaleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeSaleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeSaleAppApplication.class, args);
+	}
+
+}
